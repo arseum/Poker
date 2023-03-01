95adc5f48ad6156f18371a372341aaff265b0602
Bananabenj <benj.thibout@gmail.com>
2023-03-01 16:41:38 +0100

    début classe carte

--- src/Carte.java
+++ src/Carte.java
@@ -1,2 +1,17 @@
-public class Carte {
+public class Carte implements Constante {
+    private int valeur;
+    private String couleur;
+
+    public Carte(int valeur, String couleur){ this.couleur = couleur;  this.valeur = valeur; }
+
+    @Override
+    public String toString() {
+        return  "╔══════════╗\n" +
+                "║          ║\n" +
+                "║    "+ valeur +"    ║\n " +
+                "║   "+ couleur + "  ║\n "+
+                "║          ║\n "+
+                "╚══════════╝" ;
+    }
+    //╚ ╔  ═══║╗╝
 }
