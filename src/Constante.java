067b59615d90a795acae05793c8e40938cbf20b3
arseum45 <manbrozer@gmail.com>
2023-03-01 16:10:39 +0200

    interface

---
+++ src/Constante.java
@@ -0,0 +1,9 @@
+public interface Constante {
+
+    final String PIQUE = "pique";
+    final String CARREAU = "carreau";
+    final String TREFLE = "trefle";
+    final String COEUR = "coeur";
+
+
+}
