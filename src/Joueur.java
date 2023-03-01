eb5fb4e4fcbe20401753b91e3f22770c537eee0b
arseum45 <manbrozer@gmail.com>
2023-03-01 16:29:02 +0200

    vgyicvi

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,5 +1,18 @@
 public class Joueur {
 
+    private String pseudo;
+    private int totalJeton;
+    private int blinde; //0 = pas de blinde, 1 = petite blinde, 2 = grosse blinde
+    private boolean estCouche;
+    private Carte[] main;
+
+    public Joueur(String pseudo, int totalJeton) {
+        this.pseudo = pseudo;
+        this.totalJeton = totalJeton;
+        blinde = 0;
+        estCouche = false;
+        main = new Carte[2];
+    }
 
 
 }
--- src/Partie.java
+++ src/Partie.java
@@ -1,2 +1,24 @@
 public class Partie {
+
+    // -1 = non defini
+    private int petiteBlindeActuelle;
+    private int grosseBlindeActuelle;
+    private int compteurTour;
+    private int pot;
+    private int minimumMise;
+    private Carte[] paquet;
+    private Joueur[] joueurs;
+    private Carte[] riviere;
+
+
+    public Partie(int nombreJouer) {
+        paquet = new Carte[52];
+        joueurs = new Joueur[nombreJouer];
+        riviere = new Carte[5];
+        minimumMise = -1;
+        grosseBlindeActuelle = -1;
+        petiteBlindeActuelle = -1;
+        compteurTour = -1;
+        pot = -1;
+    }
 }
